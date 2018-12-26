05cdbace6430942435f69fa743e2d521d97ad245
darren0609 <32918735+darren0609@users.noreply.github.com>
2018-12-26 12:39:42 -0800

    Update after Section 2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# homelibrary
+# Home Library
+
 Linked In learning - Web Development and GO
+
+Built to support learning how to create my own personal Library in GO. Using Web frameworks, etc. 
+
+Starting basically, build up over time to a database managed, with multi-user mechanism to support the searching and displaying of a library. 
+
+Build from scratch in GOLANG
+
+
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Page struct {
+	Name     string
+	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year   string `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
+}
+
+func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	db, _ := sql.Open("sqlite3", "dev.db")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		p := Page{Name: "Gopher"}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+		p.DBStatus = db.Ping() == nil
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		var results []SearchResult
+		var err error
+
+		if results, err = search(r.FormValue("search")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+type ClassifySearchResponse struct {
+	BookData struct {
+		Title  string `xml:"title.attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommendations>dds>mostPopular`
+}
+
+func search(query string) ([]SearchResult, error) {
+	var resp *http.Response
+	var err error
+
+	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
+		return []SearchResult{}, err
+	}
+
+	defer resp.Body.Close()
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return []SearchResult{}, err
+	}
+
+	var c ClassifySearchResponse
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
+}
